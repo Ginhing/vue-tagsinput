c2a699661952f6899a22c2b52220f3020e9b6910
Ginhing <poke5464@gmail.com>
2016-05-03 17:55:46 +0800

    add source code

---
+++ src/example.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+new Vue({
+    el: '#example',
+    components: {
+        'tags-input': require('./input.vue')
+    }
+})
---
+++ src/input.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>hello world</div>
+</template>
+<style lang='scss'>
+#example {
+    color: cyan;
+}
+</style>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
