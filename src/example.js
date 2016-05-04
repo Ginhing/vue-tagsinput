2a18bdf05893cfa52960be1c33c96b1c7d13cdb2
Ginhing <poke5464@gmail.com>
2016-05-04 10:43:58 +0800

    inplement basic UI

--- src/example.js
+++ src/example.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 
 new Vue({
     el: '#example',
+    data() {
+        return {tags: [{text: 'hello'}, {text: 'world'}]}
+    },
     components: {
         'tags-input': require('./input.vue')
     }
--- src/input.vue
+++ src/input.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>hello world</div>
+    <div class="tags-input">
+        <tag v-for="item in tags"
+            :item="item"
+            :remove="remove">
+        </tag>
+    </div>
 </template>
-<style lang='scss'>
-#example {
-    color: cyan;
+<style lang='scss' scoped>
+.tags-input {
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.19);
+    padding: 2px;
 }
 </style>
 <script>
     export default {
-        data() {
-            return {
-
+        props: {
+            tags: {
+                twoWay: true,
+                type: Array,
+                default: () => []
             }
+        },
+        methods: {
+            remove(item) {
+                let index = this.tags.indexOf(item)
+                index > -1 && this.tags.splice(index, 1)
+            }
+        },
+        components:{
+            tag: require('./tag.vue')
         }
     }
 </script>
