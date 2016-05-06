080ebdf30c540b918cceb45c49e3b34bbfa71b63
Ginhing <poke5464@gmail.com>
2016-05-06 11:45:36 +0800

    fixed wrong indent

--- src/input.vue
+++ src/input.vue
@@ -20,76 +20,76 @@
 </style>
 <script>
 import {E} from './lib'
-    export default {
-        props: {
-            tags: {
-                twoWay: true,
-                type: Array,
-                required: true
-            },
-            insert: {
-                type: Function,
-                default: text => text
-            },
-            render: {
-                type: Function,
-                default: item => item
-            },
-            readOnly: {
-                type: Function,
-                default: item => false
-            },
-            trackBy: {
-                type: String,
-                default: '$index'
-            }
+export default {
+    props: {
+        tags: {
+            twoWay: true,
+            type: Array,
+            required: true
         },
-        computed: {
-            length() {
-                return this.tags.length
-            }
+        insert: {
+            type: Function,
+            default: text => text
         },
-        events: {
-            [E`insert`](index, text) {
-                let tag = this.insert(text)
-                !this.dedupe(tag) && this.tags.splice(index, 0, tag)
-            },
-            [E`activeOther`](index) {
-                index >= 0
-                && index <= this.length
-                && this.$broadcast(E`active`, index)
-            },
-            [E`remove`]: 'removeTag'
+        render: {
+            type: Function,
+            default: item => item
         },
-        methods: {
-            removeTag(index) {
-                if (index > -1) {
-                    let canRM = !this.readOnly(this.tags[index])
-                    canRM && this.tags.splice(index, 1)
-                }
-            },
-            inputLast() {
-                this.$broadcast(E`active`, this.length)
-            },
-            dedupe(tag) {
-                if (this.trackBy === '$index') return this.tags.includes(tag)
-                else {
-                    let field = tag[this.trackBy]
-                    return this.tags.some(item => item[this.trackBy] === field)
-                }
-            }
+        readOnly: {
+            type: Function,
+            default: item => false
         },
-        filters: {
-            getText(item) {
-                return this.render(item)
-            },
-            getRemoveHandle(item, index) {
-                return this.readOnly(item) ? null : this.removeTag.bind(this, index)
-            }
-        },
-        components:{
-            tag: require('./tag.vue'),
-            typing: require('./typing.vue')
+        trackBy: {
+            type: String,
+            default: '$index'
         }
+    },
+    computed: {
+        length() {
+            return this.tags.length
+        }
+    },
+    events: {
+        [E`insert`](index, text) {
+            let tag = this.insert(text)
+            !this.dedupe(tag) && this.tags.splice(index, 0, tag)
+        },
+        [E`activeOther`](index) {
+            index >= 0
+            && index <= this.length
+            && this.$broadcast(E`active`, index)
+        },
+        [E`remove`]: 'removeTag'
+    },
+    methods: {
+        removeTag(index) {
+            if (index > -1) {
+                let canRM = !this.readOnly(this.tags[index])
+                canRM && this.tags.splice(index, 1)
+            }
+        },
+        inputLast() {
+            this.$broadcast(E`active`, this.length)
+        },
+        dedupe(tag) {
+            if (this.trackBy === '$index') return this.tags.includes(tag)
+            else {
+                let field = tag[this.trackBy]
+                return this.tags.some(item => item[this.trackBy] === field)
+            }
+        }
+    },
+    filters: {
+        getText(item) {
+            return this.render(item)
+        },
+        getRemoveHandle(item, index) {
+            return this.readOnly(item) ? null : this.removeTag.bind(this, index)
+        }
+    },
+    components:{
+        tag: require('./tag.vue'),
+        typing: require('./typing.vue')
     }
+}
 </script>
